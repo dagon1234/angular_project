106a8443ac5fbf49c1a7fa9897926550680b4371
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-09-16 20:56:35 +0700

    add nav-bar for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ScoreListComponent } from './score/score-list.component';
+import { QuizComponent } from './quiz/quiz.component'; // Import UserComponent
+import { QuizComponentAdmin } from './admin/admin.component'; // Import AdminComponent
 
 const routes: Routes = [
-  { path: 'scores', component: ScoreListComponent },
+  { path: 'user', component: QuizComponent }, // User route
+  { path: 'admin', component: QuizComponentAdmin }, // Admin route
+  { path: '', redirectTo: '/user', pathMatch: 'full' }, // Default route
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
